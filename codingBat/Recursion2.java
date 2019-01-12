0cdd6d6aa93fd4d3502f61d4b85773f83e863f80
Dhinesh <dhinesh@live.fr>
2019-01-12 19:05:18 +0100

    get starting with recursion 2

--- codingBat/Recursion2.java
+++ codingBat/Recursion2.java
@@ -4,8 +4,47 @@ public class Recursion2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] a = { 2, 4, 8 };
-		System.out.println(groupSum(0, a, 12));
+
+		int[] a = { 2, 5, 10, 4, 2 };
+		System.out.println(groupNoAdj(0, a, 7));
+
+	}
+
+	static boolean groupNoAdj(int i, int[] nums, int target) {
+		if (target == 0) {
+			return true;
+		} else if (target < 0 || i >= nums.length || (i == nums.length - 1 && (target - nums[i]) != 0)) {
+			return false;
+		} else {
+			if (i == 0) {
+				return groupNoAdj(i + 2, nums, target - nums[i]) || groupNoAdj(i + 3, nums, target - nums[i + 1]);
+			}
+			return groupNoAdj(i + 2, nums, target - nums[i]) || groupNoAdj(i + 2, nums, target)
+					|| groupNoAdj(i + 3, nums, target - nums[i]) || groupNoAdj(i + 3, nums, target);
+		}
+
+	}
+
+	static boolean groupSum6(int i, int[] nums, int target) {
+		if (target == 0) {
+			if (i < nums.length) {
+				if (nums[i] == 6) {
+					return groupSum6(i + 1, nums, target - 6);
+				} else {
+					return groupSum6(i + 1, nums, target);
+				}
+			} else {
+				return true;
+			}
+		} else if (target < 0 || i >= nums.length) {
+			return false;
+		} else {
+			if (nums[i] == 6) {
+				return groupSum6(i + 1, nums, target - 6);
+			} else {
+				return groupSum6(i + 1, nums, target - nums[i]) || groupSum6(i + 1, nums, target);
+			}
+		}
 	}
 
 	static boolean groupSum(int i, int[] nums, int target) {
