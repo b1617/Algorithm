46f147dd59d8f2e42aeaa0d59bc8f016abe9dab9
Dhinesh <sattia.dhinesh@gmail.com>
2020-05-17 19:24:22 +0200

    add 581

---
+++ Leetcode/easy/581_Shortest_Unsorted_Continuous_Subarray.js
@@ -0,0 +1,26 @@
+/**
+ * Time complexity : O(nlogn)
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findUnsortedSubarray = function (nums) {
+  let sortedNums = nums.slice(0).sort((a, b) => a - b);
+  let x = null;
+  let y = null;
+  for (let i = 0, j = nums.length - 1; i < nums.length, j > -1; ++i, --j) {
+    if (nums[i] !== sortedNums[i] && x == null) {
+      x = i;
+    }
+    if (nums[j] !== sortedNums[j]) {
+      if (nums[j] !== sortedNums[j] && y == null) {
+        y = j + 1;
+      }
+    }
+    if (x && y) {
+      break;
+    }
+  }
+  return y != null && y != null ? y - x : 0;
+};
+
+console.log(findUnsortedSubarray([2, 6, 4, 8, 10, 9, 15]));
