6dc1c73850fe28f62581c6e6a122420eb8296b82
Dhinesh <sattia.dhinesh@gmail.com>
2020-05-18 20:58:47 +0200

    1315

---
+++ Leetcode/medium/1315_Sum_of_Nodes_with_Even-Valued_Grandparent.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * Time complexity : O(n)
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumEvenGrandparent = function (root) {
+  if (root.val === null) return 0;
+  let sum = dfs(root, null, null);
+  return sum;
+};
+
+var dfs = function (node, parent, grandParent) {
+  let leftSum = 0,
+    rightSum = 0;
+  if (node && node.val) {
+    leftSum = dfs(node.left, node, parent);
+    rightSum = dfs(node.right, node, parent);
+    return (
+      leftSum +
+      rightSum +
+      (grandParent && grandParent.val % 2 == 0 ? node.val : 0)
+    );
+  }
+  return 0;
+};
