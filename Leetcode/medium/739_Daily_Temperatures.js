1bed8f6fd2b324c5419a00bd52be3366ced784a7
Dhinesh <sattia.dhinesh@gmail.com>
2020-05-27 19:46:35 +0200

    use stack to reduce time complexity

--- Leetcode/medium/739_Daily_Temperatures.js
+++ Leetcode/medium/739_Daily_Temperatures.js
@@ -1,25 +1,18 @@
 /**
- * Time complexity : 0(n^2)
- * Space complexity : O(n)
+ * Time complexity : 0(n)
+ * Space complexity : O(w)
  * @param {number[]} T
  * @return {number[]}
  */
 var dailyTemperatures = function (T) {
-  let res = [];
-  for (let i = 0; i < T.length; ++i) {
-    const temp = T[i];
-    let flag = false;
-    for (let j = i + 1; j < T.length; ++j) {
-      const nextTemp = T[j];
-      if (nextTemp > temp) {
-        res.push(j - i);
-        flag = true;
-        break;
-      }
+  let stack = [];
+  let res = new Array(T.length).fill(0);
+  for (let i = 0; i < T.length; i++) {
+    while (stack.length && T[i] > T[stack[stack.length - 1]]) {
+      const index = stack.pop();
+      res[index] = i - index;
     }
-    if (!flag) {
-      res.push(0);
-    }
+    stack.push(i);
   }
   return res;
 };
