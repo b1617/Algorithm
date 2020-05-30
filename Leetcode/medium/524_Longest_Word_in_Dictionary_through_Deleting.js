4697d9f7095a7b66b15e48903e2d46d832245e56
Dhinesh <sattia.dhinesh@gmail.com>
2020-05-30 15:28:30 +0200

    add 524 not optimal

---
+++ Leetcode/medium/524_Longest_Word_in_Dictionary_through_Deleting.js
@@ -0,0 +1,82 @@
+/**
+ * Time complexity : O(n^2)
+ * Space complexity : O(n)
+ * @param {string} s
+ * @param {string[]} d
+ * @return {string}
+ */
+var findLongestWord = function (s, d) {
+  if (s.length == 0 || d.length == 0) return '';
+  let maps = {};
+  for (let i = 0; i < s.length; ++i) {
+    const chr = s[i];
+    if (!maps[chr]) {
+      maps[chr] = { ct: 1, index: [i] };
+    } else {
+      maps[chr].ct++;
+      maps[chr].index.push(i);
+    }
+  }
+
+  let res = null;
+  for (let i = 0; i < d.length; ++i) {
+    let copyMaps = JSON.parse(JSON.stringify(maps));
+    let lastIndex = -1;
+    const word = d[i];
+    let isPossible = true;
+    for (let j = 0; j < word.length; ++j) {
+      const chr = word[j];
+
+      if (copyMaps[chr] && copyMaps[chr].ct > 0) {
+        let indexBigger = copyMaps[chr].index.find(
+          (index) => index > lastIndex
+        );
+        if (indexBigger != null) {
+          copyMaps[chr].ct--;
+          lastIndex = indexBigger;
+          copyMaps[chr].index.splice(
+            copyMaps[chr].index.indexOf(indexBigger),
+            1
+          );
+        } else {
+          isPossible = false;
+          break;
+        }
+      } else {
+        isPossible = false;
+        break;
+      }
+    }
+    if (isPossible && word.length > 0) {
+      // check lexico small
+      if (res !== null) {
+        if (res.length < word.length) {
+          res = word;
+        } else if (res.length == word.length) {
+          for (let i = 0; i < res.length; ++i) {
+            if (word[i] < res[i]) {
+              res = word;
+            } else if (word[i] > res[i]) {
+              break;
+            }
+          }
+        }
+      } else {
+        res = word;
+      }
+    }
+  }
+  return res === null ? '' : res;
+};
+
+console.log(
+  'return',
+  findLongestWord('aewfafwafjlwajflwajflwafj', [
+    'apple',
+    'ewaf',
+    'awefawfwaf',
+    'awef',
+    'awefe',
+    'ewafeffewafewf'
+  ])
+);
