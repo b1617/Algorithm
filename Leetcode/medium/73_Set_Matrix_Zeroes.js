c5a9478787495aa4d4aecf09b0a1b14063844f8b
Dhinesh <sattia.dhinesh@gmail.com>
2020-06-01 21:10:28 +0200

    reduce complexity

--- Leetcode/medium/73_Set_Matrix_Zeroes.js
+++ Leetcode/medium/73_Set_Matrix_Zeroes.js
@@ -1,5 +1,5 @@
 /**
- * Time complexity : O(n*m) * (n+m)
+ * Time complexity : O(n*m)
  * Space complexity : O(1)
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
@@ -8,37 +8,51 @@ var setZeroes = function (matrix) {
   if (matrix.length == 0) return;
   let rows = matrix.length;
   let cols = matrix[0].length;
-  //   console.log(rows, cols
+  console.log(cols);
+  let isZeroRow = false;
+  let isZeroCol = false;
+
   for (let i = 0; i < rows; ++i) {
-    for (let j = 0; j < cols; ++j) {
-      // check 0
-      const e = matrix[i][j];
-      if (e === 0) {
-        // top to bottom
-        for (let k = 0; k < rows; ++k) {
-          matrix[k][j] = matrix[k][j] !== 0 ? 'x' : 0;
-        }
-        // left to right
-        for (let l = 0; l < cols; ++l) {
-          matrix[i][l] = matrix[i][l] !== 0 ? 'x' : 0;
-        }
+    if (matrix[i][0] == 0) {
+      isZeroRow = true;
+      break;
+    }
+  }
+
+  for (let i = 0; i < cols; ++i) {
+    if (matrix[0][i] == 0) {
+      isZeroCol = true;
+      break;
+    }
+  }
+
+  for (let i = 1; i < rows; ++i) {
+    for (let j = 1; j < cols; ++j) {
+      if (matrix[i][j] == 0) {
+        matrix[0][j] = 0;
+        matrix[i][0] = 0;
       }
     }
   }
-  for (let i = 0; i < rows; ++i) {
-    for (let j = 0; j < cols; ++j) {
-      if (matrix[i][j] === 'x') {
+
+  for (let i = 1; i < rows; ++i) {
+    for (let j = 1; j < cols; ++j) {
+      if (matrix[0][j] == 0 || matrix[i][0] == 0) {
         matrix[i][j] = 0;
       }
+    }
+  }
+  if (isZeroRow) {
+    for (let i = 0; i < rows; ++i) {
+      matrix[i][0] = 0;
+    }
+  }
+  if (isZeroCol) {
+    for (let i = 0; i < cols; ++i) {
+      matrix[0][i] = 0;
     }
   }
   return matrix;
 };
 
-console.log(
-  setZeroes([
-    [0, 1, 2, 0],
-    [3, 4, 5, 2],
-    [1, 3, 1, 5]
-  ])
-);
+console.log(setZeroes([[1, 0]]));
